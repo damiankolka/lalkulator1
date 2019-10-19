db622f80c432a54b301723bd6560ce3c71368ea8
student <damiankolka>
2019-10-19 09:08:25 +0200

    Add menu to application

---
+++ kalkulator1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kalkulator1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("0. wYJSCIE");
+                Console.WriteLine("0. dodawanie");
+                Console.WriteLine("0. odejmowanie");
+                int menuOption = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
